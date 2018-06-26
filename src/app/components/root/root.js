1331c804e161557927dda7fd4cd22e890adb3c1d
Philip Ekblom <philip.ekblom@gmail.com>
2018-06-26 20:19:59 +0200

    Cleanup

--- src/app/components/root/root.js
+++ src/app/components/root/root.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { loadCategory } from '../../actions/categoryActions';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {loadCategory} from '../../actions/categoryActions';
+import {getProductImageLink} from '../../utils/product';
 import './root.scss';
+
+import ProductField from '../product-field/product-field';
 
 class Root extends Component {
     componentDidMount() {
         this.props.dispatch(loadCategory());
     }
 
+    renderProducts() {
+        const {category} = this.props;
+        const products = category.get('products');
+        let view = '';
+        if (products) {
+            console.log(products);
+            view = products.map((product, index) =>
+                <ProductField key={index}
+                              label={product.get('name')}
+                              rating={product.get('avgRating')}
+                              stores={product.get('retailerCount')}
+                              specs={product.get('shortDescription')}
+                              img={getProductImageLink(product)}
+                              price={product.get('localMinPrice')}
+                              link={product.get('comparePricesLink')} />
+            );
+        }
+        return view;
+    }
+
     render() {
-        /*
-         * You can get the category data from the category prop:
-         * const { category } = this.props;
-         */
         return (
-            <div className="hello-world">
-                Hello world! 👋🏻
+            <div>
+                {this.renderProducts()}
             </div>
         );
     }
 }
 
 function mapStateToProps(state) {
-    const { categoryReducers } = state;
+    const {categoryReducers} = state;
 
     return {
         category: categoryReducers.get('category')
